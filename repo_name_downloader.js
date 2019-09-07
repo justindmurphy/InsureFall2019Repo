77b8012dfc95686fdc2ff2ff876b856b878909c4
Akond Rahman <akond.rahman.buet@gmail.com>
2019-09-06 23:54:36 -0500

    Repo Mining With BigQuery

---
+++ repo_name_downloader.js
@@ -0,0 +1,63 @@
+const {BigQuery} = require('@google-cloud/bigquery');
+var fs = require('fs');
+
+const projectId = "githubsolidityquery";
+const sqlQuery = 'SELECT * FROM `LOL.GHT_TRAVIS_JULIA`' ; 
+
+var out_fil = 'GHT_TRAVIS_JULIA.csv'
+
+const bigquery = new BigQuery({
+  projectId: projectId,
+  keyFilename: '/Users/akond/google-query/GithubSolidityQuery-54472c80e6dc.json', 
+  location: 'US'
+});
+
+const options = {
+  query: sqlQuery,
+  useLegacySql: false, 
+};
+
+let job;
+var fullData = '' ;
+
+
+bigquery
+  .createQueryJob(options)
+  .then(results => {
+    job = results[0];
+    console.log(`Job ${job.id} started.`);
+    return job.promise();
+  })
+  .then(() => {
+    return job.getMetadata();
+  })
+  .then(metadata => {
+    const errors = metadata[0].status.errors;
+    if (errors && errors.length > 0) {
+      throw errors;
+    }
+  })
+  .then(() => {
+    console.log(`Job ${job.id} completed.`);
+    return job.getQueryResults();
+  })
+  .then(results => {
+    const rows = results[0];
+    rows.forEach(function(row_as_json){
+      repo_name      = row_as_json['repo_name'];
+  
+      data   = repo_name + ',' + '\n' ;
+      fullData = fullData + data ; 
+    });
+
+    fs.writeFile(out_fil, fullData, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+        console.log("JuliaWithTravis data dumped succesfully ... ");
+    }); 
+
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
