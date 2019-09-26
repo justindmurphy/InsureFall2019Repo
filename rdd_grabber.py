df9df104d113785edd7b27d1cadac759b50f4f8b
Akond Rahman <akond.rahman.buet@gmail.com>
2019-09-25 21:07:52 -0500

    rdd grabber

--- rdd_grabber.py
+++ rdd_grabber.py
@@ -4,23 +4,37 @@ Sep 25, 2019
 Get RDD Dataset
 '''
 import pandas as pd 
-import datetime 
+from datetime import datetime
 import time 
+import numpy as np 
 
 # def getTravisStartTime(proj_df):
 
 def getDate(single_val): 
     date_str = single_val.split('T')[0]
     date_    = datetime.strptime(date_str, '%Y-%m-%d')
+    # print type(date_)   , detetime object 
     return date_ 
+
+def getTravisStartDate(df_pa):
+    projects = np.unique(df_pa['REPO'].tolist()) 
+    for proj_ in projects:
+        travis_mod_days = df_pa[df_pa['REPO']==proj_]['TRAVIS_MODI_DATE'].tolist() 
+        start_date      = min(travis_mod_days) 
+        print proj_, start_date  
+
 
 
 if __name__=='__main__':
    travis_ci_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/JobPrep-TNTU2019/research/Insure/Datasets/TRAVIS_START_TIME_DATE.csv'
    travis_ci_df_  = pd.read_csv(travis_ci_file) 
 
-   travis_ci_df_['DAY'] = travis_ci_df_.apply(getDate)
-   print travis_ci_df_.head() 
+   travis_ci_df_['TRAVIS_MODI_DATE'] = travis_ci_df_['TRAVIS_MODI_TIME'].apply(getDate)
+   #print travis_ci_df_.head() 
+
+   proj_travis_date = getTravisStartDate(travis_ci_df_) 
 
    proj_secu_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/JobPrep-TNTU2019/research/Insure/Datasets/UNIQUE_SECU_COMM.csv' 
    proj_secu_df_  = pd.read_csv(proj_secu_file)  
+
+
