c2fda93c18d7e45aa90421d40509d74a3f411c14
Akond Rahman <akond.rahman.buet@gmail.com>
2019-09-26 14:16:04 -0500

    rdd ci stuff

--- rdd_grabber.py
+++ rdd_grabber.py
@@ -46,7 +46,6 @@ def getBeforeAfterCIDate(travis_dict, df_, days_cutoff=60):
         # print ci_start_date
         days_before_ci, days_after_ci= len(before_ci_dates), len(after_ci_dates)
         if (days_before_ci >= days_cutoff) and (days_after_ci >= days_cutoff) :
-            valid_days_projects += 1
             sorted_days_before  = sorted(before_ci_dates, reverse=True)
             sorted_days_before  = sorted(sorted_days_before)
             sorted_days_after   = sorted(after_ci_dates)
@@ -68,6 +67,54 @@ def getBeforeAfterCIDate(travis_dict, df_, days_cutoff=60):
     print 'Projects existing:', len(proj_hash_dict)
     return proj_hash_dict 
 
+def getValuesForHash(repo_, hash_, comm_df, bug_df_, secu_df_):
+    repo_df    = comm_df[comm_df['REPO']==repo_]
+    commit_add = repo_df[repo_df['HASH']==hash_]['ADD_LOC'].tolist()[0]
+    commit_del = repo_df[repo_df['HASH']==hash_]['DEL_LOC'].tolist()[0]
+    commit_tot = repo_df[repo_df['HASH']==hash_]['TOT_LOC'].tolist()[0]    
+
+    commit_bug = bug_df_[bug_df_['HASH']==hash_]['FINAL_RATING'].tolist()[0]    
+    commit_sec = secu_df_[secu_df_['HASH']==hash_]['SECU_FLAG'].tolist()[0]    
+
+    return commit_add, commit_del, commit_tot, commit_bug, commit_sec 
+
+def getDevCountForProj(dev_df_, proj_name_):
+    return len( np.unique( dev_df_[dev_df_['REPO']==proj_name_]['DEV_IDENTIFIER'].tolist() ) )
+
+def getCommCountForProj(comm_df_, proj_name_):
+    return len( np.unique( comm_df_[comm_df_['REPO']==proj_name_]['HASH'].tolist() ) )    
+
+def getCIAdoptionDays(hash_before_list, commit_df, proj_):
+    before_date_list = []
+    commit_hash_dates =  commit_df[commit_df['REPO']==proj_]['DATE'].tolist() 
+    proj_start_date   =  min(commit_hash_dates) 
+
+    for hash_ in hash_before_list:
+        before_date_list.append( commit_df[commit_df['HASH']==hash_]['DATE'].tolist()[0] )
+    ci_start_date   =  max(before_date_list)  
+
+    print proj_, proj_start_date, ci_start_date 
+
+
+
+def generateDataset(dic_, commit_df, bug_df, secu_df, dev_df, csv_out_file):
+    for k_, v_ in dic_.iteritems(): 
+        time_after_intervention, intervention_flag  = 0, 0 
+        proj_name = k_ 
+
+        dev_count, commit_count = getDevCountForProj(dev_df, proj_name), getCommCountForProj(commit_df, proj_name)
+
+        before_hash_list, after_hash_list = v_ 
+        age_at_travis           = getCIAdoptionDays(before_hash_list, commit_df, proj_name)
+        for hash_val in before_hash_list:
+            commi_add, commi_del, commi_tot, commi_bug, commi_sec  = getValuesForHash(proj_name, hash_val, commit_df, bug_df, secu_df) 
+
+        for hash_val in after_hash_list:
+            time_after_intervention += 1 
+            intervention_flag = 1 
+            commi_add, commi_del, commi_tot, commi_bug, commi_sec  = getValuesForHash(proj_name, hash_val, commit_df, bug_df, secu_df) 
+
+
 if __name__=='__main__':
    travis_ci_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/JobPrep-TNTU2019/research/Insure/Datasets/TRAVIS_START_TIME_DATE.csv'
    travis_ci_df_  = pd.read_csv(travis_ci_file) 
@@ -81,6 +128,18 @@ if __name__=='__main__':
    proj_secu_df_  = pd.read_csv(proj_secu_file)  
    proj_secu_df_['DATE'] = proj_secu_df_['TIME'].apply(getDate)
    #print proj_secu_df_
-   all_proj_before_after_hash_dict = getBeforeAfterCIDate(proj_travis_date_dict, proj_secu_df_, 90) 
+   all_proj_before_after_hash_dict = getBeforeAfterCIDate(proj_travis_date_dict, proj_secu_df_, 60) 
 
+   comm_size_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/JobPrep-TNTU2019/research/Insure/Datasets/COMMIT_SIZE_FINAL.csv' 
+   comm_size_df_  = pd.read_csv(comm_size_file)     
+   comm_size_df_['DATE'] = comm_size_df_['TIME'].apply(getDate)
+
+   buggy_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/JobPrep-TNTU2019/research/Insure/Datasets/UNIQUE_BUG_COMM.csv' 
+   buggy_df_  = pd.read_csv(buggy_file)     
+
+   dev_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/JobPrep-TNTU2019/research/Insure/Datasets/UNIQUE_DEVS.csv' 
+   dev_df_  = pd.read_csv(dev_file)     
+
+   rdd_out_file =  '/Users/akond/Documents/AkondOneDrive/OneDrive/JobPrep-TNTU2019/research/Insure/Datasets/RDD_DATASET.csv'
+   generateDataset(all_proj_before_after_hash_dict, comm_size_df_, buggy_df_, proj_secu_df_, dev_df_ , rdd_out_file) 
 
