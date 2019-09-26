b78f4e9f490085807a5138d0d38fe2620ea621ee
Akond Rahman <akond.rahman.buet@gmail.com>
2019-09-25 21:21:17 -0500

    rdd grabber

--- rdd_grabber.py
+++ rdd_grabber.py
@@ -17,13 +17,25 @@ def getDate(single_val):
     return date_ 
 
 def getTravisStartDate(df_pa):
+    travis_dict = {}
     projects = np.unique(df_pa['REPO'].tolist()) 
     for proj_ in projects:
         travis_mod_days = df_pa[df_pa['REPO']==proj_]['TRAVIS_MODI_DATE'].tolist() 
         start_date      = min(travis_mod_days) 
-        print proj_, start_date  
+        if proj_ not in travis_mod_days:
+            travis_dict[proj_] =  start_date
+    return travis_dict 
 
 
+def getBeforeAfterCIDate(travis_dict, df_):
+    for proj_name, ci_start_date in travis_dict.iteritems():
+        proj_dates = df_[df_['REPO']==proj_name]['DATE'].tolist()
+        before_ci_dates = [x_ for x_ in proj_dates if x_ < ci_start_date ] 
+        after_ci_dates  = [x_ for x_ in proj_dates if x_ >= ci_start_date ] 
+        print ci_start_date
+        print 'Before:', before_ci_dates
+        print 'After:', after_ci_dates
+        print '='*50
 
 if __name__=='__main__':
    travis_ci_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/JobPrep-TNTU2019/research/Insure/Datasets/TRAVIS_START_TIME_DATE.csv'
@@ -32,9 +44,12 @@ if __name__=='__main__':
    travis_ci_df_['TRAVIS_MODI_DATE'] = travis_ci_df_['TRAVIS_MODI_TIME'].apply(getDate)
    #print travis_ci_df_.head() 
 
-   proj_travis_date = getTravisStartDate(travis_ci_df_) 
+   proj_travis_date_dict = getTravisStartDate(travis_ci_df_) 
 
    proj_secu_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/JobPrep-TNTU2019/research/Insure/Datasets/UNIQUE_SECU_COMM.csv' 
    proj_secu_df_  = pd.read_csv(proj_secu_file)  
+   proj_secu_df_['DATE'] = proj_secu_df_['TIME'].apply(getDate)
+   #print proj_secu_df_
+   getBeforeAfterCIDate(proj_travis_date_dict, proj_secu_df_) 
 
 
