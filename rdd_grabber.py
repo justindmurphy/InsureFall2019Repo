de7d08656eb8f37990e12fd2107a06137702a9d0
Akond Rahman <akond.rahman.buet@gmail.com>
2019-09-25 21:54:45 -0500

    rdd grabber

--- rdd_grabber.py
+++ rdd_grabber.py
@@ -27,8 +27,18 @@ def getTravisStartDate(df_pa):
     return travis_dict 
 
 
+def getHashesForDate(proj_, day_list, full_df ):
+    hash_list = []
+    proj_df =  full_df[full_df['REPO']==proj_]
+    for day_ in day_list:
+        hash_ = proj_df[proj_df['DATE']==day_]['HASH'].tolist()[0]
+        hash_list.append(hash_)
+    return hash_list 
+
+
+
 def getBeforeAfterCIDate(travis_dict, df_, days_cutoff=60):
-    valid_days_projects = 0 
+    proj_hash_dict = {}
     for proj_name, ci_start_date in travis_dict.iteritems():
         proj_dates = df_[df_['REPO']==proj_name]['DATE'].tolist()
         before_ci_dates = [x_ for x_ in proj_dates if x_ <= ci_start_date ] 
@@ -38,15 +48,25 @@ def getBeforeAfterCIDate(travis_dict, df_, days_cutoff=60):
         if (days_before_ci >= days_cutoff) and (days_after_ci >= days_cutoff) :
             valid_days_projects += 1
             sorted_days_before  = sorted(before_ci_dates, reverse=True)
-            sorted_days_after   = sorted(after_ci_dates, reverse=True)
+            sorted_days_before  = sorted(sorted_days_before)
+            sorted_days_after   = sorted(after_ci_dates)
+
             before_topk_days    = sorted_days_before[0:days_cutoff] 
             after_topk_days     = sorted_days_after[0:days_cutoff] 
-            print 'Before:', before_topk_days
-            print 'After:',  after_topk_days 
-            print '='*50
-    print '*'*100
+            # print 'Before:', before_topk_days
+            # print 'After:',  after_topk_days 
+            before_topk_hashes  = getHashesForDate(proj_name, before_topk_days, df_)
+            after_topk_hashes   = getHashesForDate(proj_name, after_topk_days, df_)
+            # print 'Before:', before_topk_hashes
+            # print 'After:',  after_topk_hashes
+            # print '='*25
+            if proj_name not in proj_hash_dict:
+                proj_hash_dict[proj_name] = (before_topk_hashes, after_topk_hashes) 
+
+    print '*'*50
     print 'Day cutoff :', days_cutoff
-    print 'Projects existing:', valid_days_projects
+    print 'Projects existing:', len(proj_hash_dict)
+    return proj_hash_dict 
 
 if __name__=='__main__':
    travis_ci_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/JobPrep-TNTU2019/research/Insure/Datasets/TRAVIS_START_TIME_DATE.csv'
@@ -61,6 +81,6 @@ if __name__=='__main__':
    proj_secu_df_  = pd.read_csv(proj_secu_file)  
    proj_secu_df_['DATE'] = proj_secu_df_['TIME'].apply(getDate)
    #print proj_secu_df_
-   getBeforeAfterCIDate(proj_travis_date_dict, proj_secu_df_, 30) 
+   all_proj_before_after_hash_dict = getBeforeAfterCIDate(proj_travis_date_dict, proj_secu_df_, 90) 
 
 
